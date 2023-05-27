2b3794e107ba119a10bee04954ccec1a7c30b20f
void <dvoid367@gmail.com>
2023-05-27 10:23:39 +0800

    first commit

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+
+// database
+abstract class DBFrame {
+    public $conn;
+    public $server;
+    public $user;
+    public $password;
+
+    abstract public function setup();
+}
+
+class Database extends DBFrame {
+    
+    public function __construct() {
+        $this->server = 'localhost';
+        $this->user = 'root';
+        $this->password = '';
+
+        $this->conn = new mysqli()
+    }
+}
