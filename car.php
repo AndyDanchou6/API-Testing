e445a4d1d311eeed9cd07c9bbb05452552a21c9a
Neil Rosite <rositeniel@gmail.com>
2023-07-02 16:17:12 +0800

    car table mod

--- car.php
+++ car.php
@@ -10,6 +10,7 @@ class Car extends Database implements Tables {
 
     public function __construct() {
         $this->setup();
+        $this->create();
     }
     public function create() {
         $create = "CREATE TABLE if not exists $this->tblName (
@@ -54,34 +55,14 @@ class Car extends Database implements Tables {
     public function show($id) {
         $get_gadget = "SELECT * FROM $this->tblName WHERE id = '$id' LIMIT 1";
         $one_item = $this->conn->query($get_gadget);
-        if ($one_item) {
-            if (mysqli_num_rows($one_item) == 1) {
-                $fetch_one = mysqli_fetch_assoc($one_item);
-                return $fetch_one;
-            }
-            else {
-                return $res = null;
-            }
-        }
-        else {
-            return $res = null;
-        }
+        $fetch_one = mysqli_fetch_assoc($one_item);
+        return $fetch_one;
     }
     public function showAll() {
         $get_all = "SELECT * FROM $this->tblName";
         $all = $this->conn->query($get_all);
-        if ($all) {
-            if (mysqli_num_rows($all) > 0) {
-                $fetch_all = mysqli_fetch_all($all);
-                return $fetch_all;
-            }
-            else {
-                return $res = null;
-            } 
-        }
-        else {
-            return $res = null;
-        }
+        $fetch_all = mysqli_fetch_all($all);
+        return $fetch_all;
     }
     public function update(array $params) {
         $id = $params['id'];
@@ -91,34 +72,13 @@ class Car extends Database implements Tables {
         $price = $params['price'];
         $manu = $params['manu'];
 
-        if (!isset($id)) {
-            return $stat = false;
+        $update = "UPDATE $this->tblName SET model_name ='$model', engine_type='$engine', drive_type='$drive', price='$price', manufacturer='$manu' WHERE id = '$id'";
+        $update_stat = $this->conn->query($update);
+        if ($update_stat) {
+            return $update_stat;
         }
-        elseif (!isset($model)) {
-            return $stat = false;
-        }
-        elseif (!isset($engine)) {
-            return $stat = false;
-        }
-        elseif (!isset($drive)) {
-            return $stat = false;
-        }
-        elseif (!isset($price)) {
-            return $stat = false;
-        }
-        elseif (!isset($manu)) {
-            return $stat = false;
-        }
-
         else {
-            $update = "UPDATE $this->tblName SET model_name ='$model', engine_type='$engine', drive_type='$drive', price='$price', manufacturer='$manu' WHERE id = '$id'";
-            $update_stat = $this->conn->query($update);
-            if ($update_stat) {
-                return $update_stat;
-            }
-            else {
-                return $stat = false;
-            }
+            return $stat = false;
         }
     }
 }
@@ -126,7 +86,6 @@ class Car extends Database implements Tables {
 
 
 $data1 = new Car();
-$data1->create();
 
 $request = $_SERVER['REQUEST_METHOD'];
 
@@ -135,48 +94,104 @@ $status = new Status();
 // create and update
 if ($request == "POST") {
     if (isset($_POST['id'])) {
-        $update = $_POST;
-        $update_stat = $data1->update($update);
-
-        if ($update_stat) {
-            $up_stats = $status->successful();
+        if ($_POST['model'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add model";
             echo json_encode($up_stats);
         }
-        else {
+        elseif ($_POST['engine'] == null) {
             $up_stats = $status->error();
+            $up_stats['solution'] = "Add engine type";
             echo json_encode($up_stats);
+        }
+        elseif ($_POST['drive'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add drive type";
+            echo json_encode($up_stats);
+        }
+        elseif ($_POST['price'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add price";
+            echo json_encode($up_stats);
+        }
+        elseif ($_POST['manu'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add manufacturer";
+            echo json_encode($up_stats);
+        }
+
+        else {
+            //$update = $_POST;
+            $update_stat = $data1->update($_POST);
+
+            if ($update_stat) {
+                $up_stats = $status->successful();
+                echo json_encode($up_stats);
+            }
+            else {
+                $up_stats = $status->error();
+                echo json_encode($up_stats);
+            }
         }
     }
+    
+// create
     else {
-        $inserted_data = $_POST;
-        $insert_stat = $data1->insert($inserted_data);
-
-        if ($insert_stat) {
-            $insert_stat = $status->created();
-            echo json_encode($insert_stat);
-            echo json_encode($inserted_data);
+        if ($_POST['model'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add model";
+            echo json_encode($up_stats);
+        }
+        elseif ($_POST['engine'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add engine type";
+            echo json_encode($up_stats);
+        }
+        elseif ($_POST['drive'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add drive type";
+            echo json_encode($up_stats);
+        }
+        elseif ($_POST['price'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add price";
+            echo json_encode($up_stats);
+        }
+        elseif ($_POST['manu'] == null) {
+            $up_stats = $status->error();
+            $up_stats['solution'] = "Add manufacturer";
+            echo json_encode($up_stats);
         }
         else {
-            $insert_stat = $status->error();
-            echo json_encode($insert_stat);
-            echo json_encode($inserted_data);
-        }
+            $inserted_data = $_POST;
+            $insert_stat = $data1->insert($inserted_data);
+
+            if ($insert_stat) {
+                $insert_stat = $status->created();
+                echo json_encode($insert_stat);
+                echo json_encode($inserted_data);
+            }
+            else {
+                $insert_stat = $status->error();
+                echo json_encode($insert_stat);
+                echo json_encode($inserted_data);
+            }
+        }   
     }
 }
-
 // read
 elseif ($request == "GET") {
     $id = json_decode($_GET['id']);
 
     if (isset($id)) {
-        $gadget_one = $data1->show($id);
-        if ($gadget_one == null) {
+        $car_one = $data1->show($id);
+        if ($car_one == null) {
             $get_stat = $status->not_found();
             echo json_encode($get_stat);
         }
         else {
             $get_stat = $status->found();
-            echo json_encode($gadget_one);
+            echo json_encode($car_one);
             echo json_encode($get_stat);
         }
     }
@@ -198,15 +213,23 @@ elseif ($request == "GET") {
 // delete
 elseif ($request == "DELETE") {
     $id = $_GET['id'];
-    $del = $data1->remove($id);
 
-    if ($del == true) {
-        $delete_stat = $status->successful();
-        echo json_encode($delete_stat);
+    if ($id == null) {
+        $del_stats = $status->error();
+        $del_stats['solution'] = "Insert id";
+        echo json_encode($del_stats);
     }
-    elseif ($del == false) {
-        $delete_stat = $status->error();
-        echo json_encode($delete_stat);
+    else {
+        $del = $data1->remove($id);
+
+        if ($del == true) {
+            $delete_stat = $status->successful();
+            echo json_encode($delete_stat);
+        }
+        elseif ($del == false) {
+            $delete_stat = $status->error();
+            echo json_encode($delete_stat);
+        }
     }
 }
 // method not allowed
